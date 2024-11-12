46a4550b04c00b1a16fb76eae815fbf02de9fa0e
SSPMADE <sentpet@gmail.com>
2024-11-12 11:38:51 +0300

    Add files via upload

--- list.js
+++ list.js
@@ -2,10 +2,35 @@ var list = {
 
 	"About": {
 		"Ssentongo Simon Peter": [
-			[ "Who is SSP", "manual/SSP001JS/Who-is-SSP" ],
-			[ "sspos", "manual/SSPOSF/sspos" ]
+			[ "Introduction", "manual/SSP001JS/Who-is-SSP" ],
+			[ "SSP OS Project", "manual/SSPOSF/sspos" ],
+			[ "SSP AI Project", "manual/SSP001AI/sspai0" ]
 		]
 		},
+
+	"Artificial Inteligence": {
+		"Machine Learning": [
+			[ "ML Introduction", "manual/SSP001AI/ML-Introduction" ],
+			[ "ML and AI", "manual/SSP001AI/ML-and-AI" ],
+			[ "ML Languages", "manual/SSP001AI/ML-Languages" ],
+			[ "ML JavaScript", "manual/SSP001AI/ML-JavaScript" ],
+			[ "ML Examples", "manual/SSP001AI/ML-Examples" ],
+			[ "ML Linear Graphs", "manual/SSP001AI/ML-Linear-Graphs" ],
+			[ "ML Scatter Plots", "manual/SSP001AI/ML-Scatter-Plots" ],
+			[ "ML Perceptrons", "manual/SSP001AI/ML-Perceptrons" ],
+			[ "ML Recognition", "manual/SSP001AI/ML-Recognition" ],
+			[ "ML Training", "manual/SSP001AI/ML-Training" ],
+			[ "ML Testing", "manual/SSP001AI/ML-Testing" ],
+			[ "ML Learning", "manual/SSP001AI/ML-Learning" ],
+			[ "ML Terminology", "manual/SSP001AI/ML-Terminology" ],
+			[ "ML Data", "manual/SSP001AI/ML-Data" ],
+			[ "ML Clustering", "manual/SSP001AI/ML-Clustering" ],
+			[ "ML Regressions", "manual/SSP001AI/ML-Regressions" ],
+			[ "ML Deep Learning", "manual/SSP001AI/ML-Deep-Learning" ],
+			[ "ML Brain js", "manual/SSP001AI/ML-Brain-js" ]
+		]
+		},
+		
 	"WEB SITE": {
 		"HTML": [
 			[ "HTML Introduction", "manual/SSP001HTML/HTML-Introduction" ],
@@ -20,9 +45,22 @@ var list = {
 			[ "Display HTML code", "manual/SSP001HTML/Display-HTML-code" ],
 			[ "Three line icon code", "manual/SSP001HTML/Three-line-icon-code" ],
 			[ "HTML Canvas", "manual/SSP001HTML/HTML-Canvas" ],
-			[ "HTML Video", "manual/SSP001HTML/HTML-Video" ]
-		        ],
-		"JAVA SCRIPT EXAMPLES": [
+			[ "HTML Video", "manual/SSP001HTML/HTML-Video" ]	
+		],
+		"CSS3": [
+			[ "Image blur effect", "manual/SSP001CSS3/Image-blur-effect" ],
+			[ "Image grayscale effect", "manual/SSP001CSS3/Image-grayscale-effect" ],
+			[ "Animated Button", "manual/SSP001CSS3/Animated-Button" ],
+			[ "Animated Button Ripple Effect", "manual/SSP001CSS3/Animated-Button-Ripple-Effect" ],
+			[ "Animated Buttons Pressed Effect", "manual/SSP001CSS3/Animated-Buttons-Pressed-Effect" ],
+			[ "Import css", "manual/SSP001CSS3/Import-css" ],
+			[ "Appendix 0", "manual/SSP001CSS3/Appendix-0" ]
+		],
+		"JAVA SCRIPT (JS)": [
+			[ "Introduction to Java Script", "manual/SSP001JS/Introduction-to-Java-Script" ]
+		],
+		"JS EXAMPLES": [
+			[ "Introduction to Java Script", "manual/SSP001JS/Introduction-to-Java-Script" ],
 			[ "Animated Search Form", "manual/SSP001JS/Animated-Search-Form" ],
 			[ "Accordion", "manual/SSP001JS/Accordion" ],
 			[ "Responsive Topnav", "manual/SSP001JS/Responsive-Topnav" ],
@@ -40,6 +78,7 @@ var list = {
 			[ "Image Slide Show", "manual/SSP001JS/Image-Slide-Show" ],
 			[ "Image-Slide-Show-auto", "manual/SSP001JS/Image-Slide-Show-auto" ],
 			[ "Calculator", "manual/SSP001JS/Calculator" ],
+			[ "simple calendar", "manual/SSP001JS/simple-calendar" ],
 			[ "Popups", "manual/SSP001JS/Popups" ],
 			[ "Notes", "manual/SSP001JS/Notes" ],
 			[ "Tabs", "manual/SSP001JS/Tabs" ],
@@ -60,25 +99,59 @@ var list = {
 			[ "Draw a triangle", "manual/SSP001JS/draw-a-triangle" ],
 			[ "Confirm dialog box", "manual/SSP001JS/Dialog-box-confirm" ],
 			[ "Alart dialog box", "manual/SSP001JS/Dialog-box-alart" ],
+			[ "Prompt dialog box", "manual/SSP001JS/Dialog-box-prompt" ],
+			
 			[ "Canvas Clock", "manual/SSP001JS/Canvas-Clock" ],
 			[ "Canvas Snake Game", "manual/SSP001JS/Canvas-Snake-Game" ],
 			[ "Canvas Game 1", "manual/SSP001JS/Canvas-Game-1" ],
 			[ "Canvas Game 1 keyboad", "manual/SSP001JS/Canvas-Game-1-keyboad" ],
 			[ "Game Pad", "manual/SSP001JS/Game-Pad" ],
 			[ "Geolocation", "manual/SSP001JS/Geolocation" ],
-			[ "Prompt dialog box", "manual/SSP001JS/Dialog-box-prompt" ],
 			[ "Paralax", "manual/SSP001JS/Paralax" ],
 			[ "Drag And Drop", "manual/SSP001JS/Drag-And-Drop" ],
 			[ "Read xml online", "manual/SSP001JS/Read-xml-online" ],
 			[ "Read json online", "manual/SSP001JS/Read-json-online" ],
 			[ "Arrays", "manual/SSP001JS/Arrays" ],
+			[ "Execute win Apps", "manual/SSP001JS/Execute-win-Apps"],
 			[ "3d using threejs", "manual/SSP001JS/3d-using-threejs" ],
 			[ "3d using threejs ssp obj", "manual/SSP001JS/3d-using-threejs-ssp-obj"],
 			[ "3d using threejs ssp obj mtl", "manual/SSP001JS/3d-using-threejs-ssp-obj-mtl" ],
 			[ "3d using threejs loader json blender", "manual/SSP001JS/3d-using-threejs-loader-json-blender"],
 			[ "3d tour", "manual/SSP001JS/3d-tour"]
-
-	                ]
+		],
+		"Flash Action Script": [
+			[ "Flash Player", "manual/SSP001AS/Flash-Action-Script-Media-Player" ]
+		],
+		"Php": [
+			[ "PHP Introduction", "manual/SSP001PHP/PHP-Introduction" ]
+		]
+		},
+		
+	"PYTHON": {
+		"Introduction": [
+			[ "Introduction To Python", "manual/SSP001Python/Introduction-To-Python" ]
+		],
+		"Examples": [
+			[ "SSP text editer", "manual/SSP001Python/SSP-text-editer" ]
+		]
+		},
+		
+	"MACHINE CODE": {
+		"ASM": [
+			[ "Introduction", "manual/SSP001ASM/asm1" ]
+		]
+		},
+		
+	"Other": {
+		"Accounting": [
+			[ "Introduction", "manual/SSP001Acc/FINANCIAL-ACCOUNTING" ]
+		],
+		"Security": [
+			[ "Security guard notes", "manual/SSP001SECURITY/Security-guard-notes" ]
+		],
+		"SSP CORPORATE": [
+			[ "Website Template", "manual/SSP001CORPORATE/index" ]
+		]
 		}
 
 };
