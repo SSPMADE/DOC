ef3030a8058f43141562310cbd412391fb03f713
SSPMADE <sentpet@gmail.com>
2024-12-17 21:30:41 +0300

    Add files via upload

---
+++ manual/SSP001CORPORATE/assets/js/sspcorporate1.js
@@ -0,0 +1,10 @@
+﻿//****ssp mod
+function ToggleMenuFunction() {
+    var x = document.getElementById("sspTogleNav");
+    if (x.className === "navbar-collapseable") {
+        x.className += " SSPresponsive";
+    } else {
+        x.className = "navbar-collapseable";
+    }
+}
+//****ssp mod
